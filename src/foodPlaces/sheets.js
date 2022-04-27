76a88fb0552fcd80f0e52bbad680cfa3496202fa
Stanico Knowles <stanicoknowles@nib-bahamas.com>
2022-04-27 14:54:39 -0400

    initial commit, get random food place

---
+++ src/foodPlaces/sheets.js
@@ -0,0 +1,24 @@
+const Sheets = require('node-sheets').default;
+const gs = new Sheets(process.env.SHEET_ID)
+
+module.exports.getFoodPlaces = async (criteria) => {
+    await gs.authorizeApiKey(process.env.GOOGLE_API_KEY);
+    const rows = await gs.tables('A:E').rows
+    const foodResults = []
+
+    for (let i = 0; i < rows.length; i++) {
+        let rating = rows[i]['Food Average Rating'].value
+        let foodPlace = rows[i]['Food Places'].value
+        let price = rows[i]['Price'].value
+        let distance = rows[i]['Distance from Office'].value
+
+        if (rating >= criteria.avgRating && price <= criteria.priceKey && distance <= criteria.distanceKey) {
+            foodResults.push(foodPlace)
+        }
+    }
+    return randomizer(foodResults)
+}
+
+const randomizer = (foodPlaces) => {
+    return foodPlaces[Math.floor((Math.random() * (foodPlaces.length)))]
+}
