d6c6d7025133eb7bb19bc1c108b2ec97bc16f82b
Stanico Knowles <65465649+Stanico-Knowles@users.noreply.github.com>
2022-04-27 16:28:30 -0400

    add validation for criteria

---
+++ src/foodPlaces/enums/customErrors.js
@@ -0,0 +1,7 @@
+const CustomErrors = {
+    AvgRatingRequired: 'Please enter average rating.',
+    DistanceKeyRequired: 'Please enter distance key.',
+    PriceKeyRequired: 'Please enter price key.',
+}
+
+module.exports = CustomErrors;
--- src/foodPlaces/sheets.js
+++ src/foodPlaces/sheets.js
@@ -1,8 +1,15 @@
+const HTTPError = require('../utils/errors/httpError');
+const CustomErrors = require('./enums/customErrors');
 const Sheets = require('node-sheets').default;
 const gs = new Sheets(process.env.SHEET_ID)
 
+/**
+ * 
+ * @param {object} criteria 
+ * @returns object
+ */
 module.exports.getFoodPlaces = async (criteria) => {
-
+    validateCriteria(criteria)
     await gs.authorizeApiKey(process.env.GOOGLE_API_KEY);
     const table = await gs.tables('A:E')
     const rows = table.rows
@@ -21,6 +28,21 @@ module.exports.getFoodPlaces = async (criteria) => {
     return { foodPlace: randomizer(foodResults) }
 }
 
+/**
+ * 
+ * @param {object} foodPlaces 
+ * @returns string
+ */
 const randomizer = (foodPlaces) => {
     return foodPlaces[Math.floor((Math.random() * (foodPlaces.length)))]
+}
+
+/**
+ * 
+ * @param {object} criteria 
+ */
+const validateCriteria = (criteria) => {
+    if (!criteria.avgRating) throw HTTPError.badRequest(CustomErrors.AvgRatingRequired)
+    if (!criteria.distanceKey) throw HTTPError.badRequest(CustomErrors.DistanceKeyRequired)
+    if (!criteria.priceKey) throw HTTPError.badRequest(CustomErrors.PriceKeyRequired)
 }
